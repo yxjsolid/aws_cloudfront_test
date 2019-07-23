24296ab079591c68495b23e1d6a293c16fe696f7
Jerry Yang <xyang@sonicwall.com>
2019-07-23 19:26:20 +0800

    update

--- functions/test_lambda.py
+++ functions/test_lambda.py
@@ -1,8 +1,9 @@
 import json
+import time
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from test!')
+        'body': json.dumps('Hello from test %s !' % time.time())
     }
